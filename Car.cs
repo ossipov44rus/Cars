df8c86200f61b34e32b7f1dfe79941e522238a0c
Alexandr Osipov <ossipov44rus@gmail.com>
2022-07-25 17:50:31 +0300

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Cars
+{
+    public class Car : IEquatable<Car?>
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Car);
+        }
+
+        public bool Equals(Car? other)
+        {
+            return other != null &&
+                   Model == other.Model &&
+                   Color == other.Color;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Model, Color);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Cars;
+
+List<Car> myCars = new List<Car>
+{
+    new Car{Model = "BMW 745i", Color="Red",Description="A little scrath on the window."},
+    new Car{Model="Mersedes s350",Color="Black", Description="Some issues with the engine."},
+    new Car{Model="Audi A4",Color="White",Description="Need to change the oil."}
+};
+Car myNewCar = new Car { Model = "BMW 745i", Color = "Red", Description = "333 h.p." };
+Console.WriteLine(myCars[0].Equals(myNewCar));
+Console.WriteLine(myCars.GetHashCode());
+Console.WriteLine(myNewCar.GetHashCode());
